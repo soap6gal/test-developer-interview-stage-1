e9f971b7a1aed6173d6602eaed595d5d6c56ee5c
bsimhadri <bsimhadri@ebay.com>
2017-08-13 15:22:37 +0100

    fixed broken tests and implemented two new test scenario's
    fixed broken tests and implemented two new test scenario's

--- src/test/java/com/db/am/bauhaus/project/steplib/SearchUser.java
+++ src/test/java/com/db/am/bauhaus/project/steplib/SearchUser.java
@@ -1,6 +1,7 @@
 package com.db.am.bauhaus.project.steplib;
 
 import com.db.am.bauhaus.project.pages.MainSearchPage;
+import com.db.am.bauhaus.project.pages.WomenFashionShopProductPage;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 
@@ -13,12 +14,36 @@ import static org.hamcrest.Matchers.containsString;
 public class SearchUser extends ScenarioSteps {
 
     MainSearchPage mainSearchPage;
+    WomenFashionShopProductPage womenFashionShopProductPage;
 
     String searchText = "craft";
+    String[] categoriesList = {"Weddings", "Clothing", "Dresses"};
+    String productSearchText = "Women's fashion shop";
 
     @Step
     public void search_from_input_box() {
         mainSearchPage.searchFromInputBox(searchText);
+    }
+
+    @Step
+    public void selects_a_category_from_dropdown_menu(){
+        mainSearchPage.selectCategories(categoriesList);
+    }
+
+    @Step
+    public void selects_a_category_from_icon(){
+        mainSearchPage.scrollDownOrUp(600);
+        mainSearchPage.clickOnCategoryFromProductIcon();
+    }
+
+    @Step
+    public void verify_result_for_category_search_dropdown_grid(){
+        assertThat(mainSearchPage.getHeaderForCategorySearch(), containsString(categoriesList[2]));
+    }
+
+    @Step
+    public void verify_result_for_category_search_product_icon(){
+        assertThat(womenFashionShopProductPage.getTitleForProductCategorySearch(), containsString(productSearchText));
     }
 
     @Step
--- src/test/resources/features/search.feature
+++ src/test/resources/features/search.feature
@@ -14,8 +14,14 @@ Feature: Sample
 
   @ui @wip
   Scenario: Should be able to search for a product from the drop-down menu
+    Given John is viewing the Etsy landing page
+    When he selects a category from drop-down menu(Grid view)
+    Then results should be displayed for selected category
 
   @ui @wip
   Scenario: Should be able to search for a product from the icons
+    Given John is viewing the Etsy landing page
+    When he selects a category from the icon
+    Then results should be displayed for selected icon
 
 
